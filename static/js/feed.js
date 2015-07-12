57b6c09c9156c0344d446905a02f490ebb58442c
budparr <budparr@sonnetmedia.net>
2015-07-12 10:35:41 -0400

    kimono change feed
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- static/js/feed.js
+++ static/js/feed.js
@@ -1,5 +1,5 @@
   $.ajax({
-  url:"https://www.kimonolabs.com/api/cxy6x3cw?apikey=70362f2a05a633871d07f1faeacd7e36&kimlimit=3",
+  url:"https://www.kimonolabs.com/api/5ntk8dfc?apikey=70362f2a05a633871d07f1faeacd7e36&kimlimit=3",
   crossDomain: true,
   dataType: "jsonp",
     success: function (response) {    
@@ -8,7 +8,7 @@
           var collection = response.results.collection1;
             for (var i = 0; i < collection.length; i++){
               // Traverses through every element in the entire collection
-              $(".list-group").append('<li class="list-group-item"><a href="' + collection[i].property1.href + '" target="_blank">' + collection[i].property1.text + '</a></li>');
+              $(".list-group").append('<li class="list-group-item"><a href="' + collection[i].title.href + '" target="_blank">' + collection[i].title.text + '</a></li>');
                 // adds the text and the links from the first property into the list
               }
 
