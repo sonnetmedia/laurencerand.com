86b327e72b882cae8dd9b984c711a2340d3c5035
budparr <budparr@sonnetmedia.net>
2015-07-12 09:23:20 -0400

    working on API
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- static/js/dist/scripts.js
+++ static/js/dist/scripts.js
@@ -4,26 +4,33 @@
   dataType: "jsonp",
   success: function (response) {    
           console.log(response);
-          $.each(results.response.posts, function(i, item){
+          $.each(response, function(i, item){
               
               
               var text = item.text;
-              var description = item.description;
+              var href = item.href;
               
               
               //$("<img/>").attr("src", src).appendTo("#wrapper").wrap('<div class="postImage"></div>').after('<span class="postCaption">' + body + '</div>');
-              if(item.text && item.href) {
-                  $('<h4><a href="' + item.href +   '" target="_blank">' +  item.text + '</a> <small><i class="icon-link-ext" style="font-size:.625em;"></i></small> </h4>').appendTo('#content_feed');
+              if(text && href) {
+
+                  $('<h4><a href="' + href +   '" target="_blank">' +  text + '</a></h4>').appendTo('#content_feed');
                     }
+
                     else if(item.text) {
                       $('<h4>' + item.title + '</h4>').appendTo('#content_feed');
+                
                 }
                 
                     $('<hr />').appendTo('#content_feed');
+              
+
+
               });
             
   },
   error: function (xhr, status) {
     //handle errors
+    console.log(response);
   }
 });
