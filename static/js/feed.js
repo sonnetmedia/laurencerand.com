4534dbb95812873a9dd207bbdb82b6258b8b6a92
budparr <budparr@sonnetmedia.net>
2015-07-12 10:00:25 -0400

    kimono ftw
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- static/js/feed.js
+++ static/js/feed.js
@@ -1,35 +1,19 @@
   $.ajax({
-  url:"https://www.kimonolabs.com/api/cxy6x3cw?apikey=70362f2a05a633871d07f1faeacd7e36",
+  url:"https://www.kimonolabs.com/api/cxy6x3cw?apikey=70362f2a05a633871d07f1faeacd7e36&kimlimit=3",
   crossDomain: true,
   dataType: "jsonp",
-  success: function (response) {    
-          console.log(response);
-          $.each(response, function(i, item){
-              
-              
-              var text = item.text;
-              var href = item.href;
-              
-              
-              //$("<img/>").attr("src", src).appendTo("#wrapper").wrap('<div class="postImage"></div>').after('<span class="postCaption">' + body + '</div>');
-              if(text && href) {
+    success: function (response) {    
+          // console.log(response);
+          $(".panel-heading").html(response.name);
+          var collection = response.results.collection1;
+            for (var i = 0; i < collection.length; i++){
+              // Traverses through every element in the entire collection
+              $(".list-group").append('<li class="list-group-item"><a href="' + collection[i].property1.href + '" target="_blank">' + collection[i].property1.text + '</a></li>');
+                // adds the text and the links from the first property into the list
+              }
 
-                  $('<h4><a href="' + href +   '" target="_blank">' +  text + '</a></h4>').appendTo('#content_feed');
-                    }
-
-                    else if(item.text) {
-                      $('<h4>' + item.title + '</h4>').appendTo('#content_feed');
-                
-                }
-                
-                    $('<hr />').appendTo('#content_feed');
-              
-
-
-              });
-            
-  },
-  error: function (xhr, status) {
+            },
+        error: function (xhr, status) {
     //handle errors
     console.log(response);
   }
